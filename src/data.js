60a97e4c9b71ce6931da666c14c1631a36f536e9
Victor Furustubbe <mantykanto2@gmail.com>
2025-05-27 13:49:35 +0200

    added animalsdata and images

--- src/data.js
+++ src/data.js
@@ -1,2 +1,9 @@
 export const links = ['Home', 'Mammals', 'Birds', 'Reptiles']
 
+// export const links = [
+//   { name: 'Home', paths: '/' },
+//   { name: 'Mammals', paths: '/mammals' },
+//   { name: 'Birds', paths: '/birds' },
+//   { name: 'Reptiles', paths: '/reptiles' }
+// ];
+
