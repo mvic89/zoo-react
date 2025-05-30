92de3793df9012f0d25775c2aeba173ad18b9d41
Victor Furustubbe <mantykanto2@gmail.com>
2025-05-31 01:06:47 +0200

    added changes

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -2,7 +2,8 @@ import { Outlet } from "react-router-dom";
 import Header from "../../components/Header";
 import styles from './layout.module.css'
 import Footer from "../../components/Footer";
-import Sidebar from "../../components/Sidebar/Index.jsx";
+import Sidebar from "../../components/Sidebar/Index";
+
 
 const Layout = () => {
     return (
